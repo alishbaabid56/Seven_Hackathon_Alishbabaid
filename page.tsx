504547c0e28888973b891d9a9ccfd8facdc4195c
Alishba Abid <164169188+alishbaabid56@users.noreply.github.com>
2025-01-21 22:36:48 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,16 @@
+import PickDropSection from "@/components/PickDropSection";
+import HeroSection from "@/components/Hero";
+import FetchCarsPage from "./homePage/page";
+
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection/>
+      <PickDropSection/>
+      <FetchCarsPage/>
+      
+      
+    </div>
+  );
+}
